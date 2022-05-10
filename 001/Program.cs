a6c17cf8d07b999b0901b779b1d7dfbfe8f159a0
Alexandra Zueva <guseva-1992@yandex.ru>
2022-05-10 22:57:09 +0300

    add solution 001

---
+++ 001/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 1
+//Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] array = new int[7];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 999);
+    Console.Write(array[i] + " ");
+}
+
+int count = 0;
+for (int j = 0; j < array.Length; j++)
+{
+    if (array[j] % 2 == 0)
+    {
+        count = count + 1;
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Even numbers:" + count);
