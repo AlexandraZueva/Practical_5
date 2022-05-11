b7da402445816a4c5f0065954cd857f8a5f753e1
Alexandra Zueva <guseva-1992@yandex.ru>
2022-05-11 22:57:23 +0300

    add solution 003

---
+++ 003/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 3: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+double[] array = { 2.4, 3.1, 7.8, 1.7 };
+double max = array[0];
+double min = array[0];
+
+for (int j = 0; j < array.Length; j++)
+{
+    if (array[j] > max) max = array[j];
+    if (array[j] < min) min = array[j];
+}
+
+Console.Write(max - min);
